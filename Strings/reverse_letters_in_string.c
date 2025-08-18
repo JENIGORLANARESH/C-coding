57137e00e9913bb0264f9dfe382973e6ceef4073
JenigorlaNaresh <nareshjenigorla@gmail.com>
2025-08-18 15:14:01 +0530

    Update reverse_letters_in_string.c

--- Strings/reverse_letters_in_string.c
+++ Strings/reverse_letters_in_string.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-	char arr[100];
-	printf("Enter string : \n");
-	fgets(arr, 100, stdin);
+char *reverse(char *t);
 
-	char *t;
-	t = strtok(arr, " ");
-	while(t != NULL){
-		printf("%s    %s\n", t , reverse(t));
-		t = strtok(NULL, " ");
-	}
-	return 0;
+int main(void){
+    char arr[100];
+    printf("Enter string : \n");
+    fgets(arr, 100, stdin);
+    arr[strcspn(arr, "\n")] = '\0';
+
+    char *t;
+    t = strtok(arr, " ");
+    while(t != NULL){
+        printf("%s ",  reverse(t));
+        t = strtok(NULL, " ");
+    }
+    printf("\n");
+    return 0;
 }
 
 char *reverse(char *t){
-	int len = strlen(t);
-	int i=0, j=len-1;
-	while(i < j){
-		char *p = t[j];
-		t[j] = t[i];
-		t[i] = p;
-	}
-	return t;
+    int len = strlen(t);
+    int i=0, j=len-1;
+    while(i < j){
+        char p = t[j];
+        t[j] = t[i];
+        t[i] = p;
+        i++;
+        j--;
+    }
+    return t;
 }
-
